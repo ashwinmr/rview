c49793486a542bf6f2e2b94068c723adacfb03bc
ashwinmr <iashwinrao@gmail.com>
2018-05-16 19:46:35 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,28 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+const url = require('url')
+
+// Start the program when app is ready
+app.on('ready', function createWindow() {
+    // Create the browser window.
+    win = new BrowserWindow({ width: 800, height: 600 })
+
+    // and load the index.html of the app.
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, 'src', 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }))
+
+    // Open the DevTools.
+    // win.webContents.openDevTools()
+})
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
