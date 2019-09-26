6813828e3187d12024e86b7ddbad44f803e21b27
ashwinmr <iashwinrao@gmail.com>
2019-09-25 22:23:24 -0700

    update html

--- app/main.js
+++ app/main.js
@@ -56,8 +56,6 @@ app.on('ready', function createWindow() {
     })
 })
 
-
-
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
     // On macOS it is common for applications and their menu bar
