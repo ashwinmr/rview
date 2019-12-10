c2b36bf417ee104748142eae9e272324e7d7abe6
ashwinmr <iashwinrao@gmail.com>
2019-12-10 19:06:01 +0530

    make app name automatic

--- app/main.js
+++ app/main.js
@@ -15,6 +15,7 @@ console.log('cpp addon: ', example_addon.hello("from cpp"));
 app.on('ready', function createWindow() {
     // Create the browser window.
     win = new BrowserWindow({
+        title: app.name,
         show: false, // Show and maximize later
         icon: path.join(__dirname, 'assets', 'icons', 'main_icon.png'),
         resizable: true,
